a550aa38ebeb5e31881a6a01dc3d4853fe3f0606
Sergey Davydov <sddevelop@ya.ru>
2023-12-25 15:01:39 +0300

    separate routes (no 405 for unknown metric type)

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/MrSwed/go-musthave-metrics/internal/constants"
 	"github.com/MrSwed/go-musthave-metrics/internal/service"
 	"github.com/go-chi/chi/v5"
@@ -16,14 +15,19 @@ func Handler(s *service.Service) {
 	//r.Use(middleware.URLFormat)
 	//r.Route(fmt.Sprintf(`%s/{metricType:%s|%s}/{metricName}/{metricValue}`,
 	//	constants.UpdateRoute, constants.MetricTypeCounter, constants.MetricTypeCounter),
-	r.Route(fmt.Sprintf(`%s/{metricType}/{metricName}/{metricValue}`, constants.UpdateRoute),
-		UpdateHandler(s))
+	r.Route(constants.UpdateRoute+"/gauge", UpdateGaugeHandler(s))
+	r.Route(constants.UpdateRoute+"/counter", UpdateCounterHandler(s))
 
 	log.Fatal(http.ListenAndServe(`:8080`, r))
 }
 
-func UpdateHandler(s *service.Service) func(r chi.Router) {
+func UpdateGaugeHandler(s *service.Service) func(r chi.Router) {
 	return func(r chi.Router) {
-		r.Post("/", UpdateMetric(s))
+		r.Post("/{metricName}/{metricValue}", UpdateGaugeMetric(s))
+	}
+}
+func UpdateCounterHandler(s *service.Service) func(r chi.Router) {
+	return func(r chi.Router) {
+		r.Post("/{metricName}/{metricValue}", UpdateCounterMetric(s))
 	}
 }
--- internal/handler/update.go
+++ internal/handler/update.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"github.com/MrSwed/go-musthave-metrics/internal/constants"
 	"github.com/MrSwed/go-musthave-metrics/internal/service"
 	"github.com/go-chi/chi/v5"
 	"log"
@@ -9,45 +8,49 @@ import (
 	"strconv"
 )
 
-func UpdateMetric(s *service.Service) func(w http.ResponseWriter, r *http.Request) {
+func UpdateGaugeMetric(s *service.Service) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		action, metricKey, metricValStr := chi.URLParam(r, "metricType"), chi.URLParam(r, "metricName"), chi.URLParam(r, "metricValue")
-		switch action {
-		case constants.MetricTypeGauge:
-			if v, err := strconv.ParseFloat(metricValStr, 64); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Error: %s", err)
-				return
-			} else {
-				if err = s.SetGauge(metricKey, v); err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					log.Printf("Error set gauge %s", err)
-				} else {
-					newV, _ := s.GetGauge(metricKey)
-					log.Printf("Stored gauge %s = %f", metricKey, newV)
-				}
-			}
-		case constants.MetricTypeCounter:
-			if v, err := strconv.ParseInt(metricValStr, 10, 64); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Error: %s", err)
-				return
-			} else {
-				if err = s.IncreaseCounter(metricKey, v); err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					log.Printf("Error set counter %s", err)
-				} else {
-					newV, _ := s.GetCounter(metricKey)
-					log.Printf("Counter updated %s = %d", metricKey, newV)
-				}
-			}
-		default:
+		metricKey, metricValStr := chi.URLParam(r, "metricName"), chi.URLParam(r, "metricValue")
+		if v, err := strconv.ParseFloat(metricValStr, 64); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			log.Printf("Error: unknown metric type '%s'", metricKey)
+			log.Printf("Error: %s", err)
 			return
+		} else {
+			if err = s.SetGauge(metricKey, v); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error set gauge %s", err)
+			} else {
+				newV, _ := s.GetGauge(metricKey)
+				log.Printf("Stored gauge %s = %f", metricKey, newV)
+			}
 		}
 
+		// if ok
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write([]byte("Saved: Ok")); err != nil {
+			log.Printf("Error: %s", err)
+		}
+	}
+}
+
+func UpdateCounterMetric(s *service.Service) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		metricKey, metricValStr := chi.URLParam(r, "metricName"), chi.URLParam(r, "metricValue")
+		if v, err := strconv.ParseInt(metricValStr, 10, 64); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Error: %s", err)
+			return
+		} else {
+			if err = s.IncreaseCounter(metricKey, v); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error set counter %s", err)
+			} else {
+				newV, _ := s.GetCounter(metricKey)
+				log.Printf("Counter updated %s = %d", metricKey, newV)
+			}
+		}
 		// if ok
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
