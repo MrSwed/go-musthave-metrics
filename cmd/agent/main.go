ef1c378a1b4c44a9b026921db8c6e76fd7a613cf
Sergey Davydov <sddevelop@ya.ru>
2023-12-26 22:05:59 +0300

    refactors

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,27 +2,8 @@ package main
 
 import (
 	"log"
-	"os"
-	"strconv"
 	"time"
 )
-
-func getEnv() {
-	addressEnv, reportIntervalEnv, pollIntervalEnv := os.Getenv("ADDRESS"), os.Getenv("REPORT_INTERVAL"), os.Getenv("POLL_INTERVAL")
-	if addressEnv != "" {
-		serverAddress = addressEnv
-	}
-	if reportIntervalEnv != "" {
-		if v, err := strconv.ParseInt(reportIntervalEnv, 10, 64); err != nil {
-			reportInterval = int(v)
-		}
-	}
-	if pollIntervalEnv != "" {
-		if v, err := strconv.ParseInt(pollIntervalEnv, 10, 64); err != nil {
-			pollInterval = int(v)
-		}
-	}
-}
 
 func main() {
 	parseFlags()
