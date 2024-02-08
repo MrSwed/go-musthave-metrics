4dbade33f16a7b0e0176e636c84036b3b2b0baf6
Sergey Davydov <sddevelop@ya.ru>
2024-02-08 13:17:37 +0300

    tidy

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -19,6 +19,15 @@ type RepositoryStorage struct {
 	DBStorage
 }
 
+type DataStorage interface {
+	SetGauge(k string, v float64) error
+	SetCounter(k string, v int64) error
+	GetGauge(k string) (float64, error)
+	GetCounter(k string) (int64, error)
+	GetAllCounters() (map[string]int64, error)
+	GetAllGauges() (map[string]float64, error)
+}
+
 func NewRepository(c *config.StorageConfig, db *sqlx.DB) (*RepositoryStorage, error) {
 	return &RepositoryStorage{
 		MemStorage:  NewMemRepository(),
