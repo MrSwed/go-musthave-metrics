6645b7ba5ccd74b8d145945a47c2c5af20748f1d
Sergey Davydov <sddevelop@ya.ru>
2024-02-22 17:22:19 +0300

    docs

--- internal/agent/error/error.go
+++ internal/agent/error/error.go
@@ -12,6 +12,7 @@ var (
 	ErrBadMetricType   = errors.New("unknown metric type")
 )
 
+// ErrWrap wrap error with debug info: line and file name where it happened
 func ErrWrap(err error) error {
 	_, fn, line, _ := runtime.Caller(1)
 	return fmt.Errorf("%w at %s:%d", err, fn, line)
