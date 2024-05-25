2dd913007a845f323b49a781ce447cae12961ed0
Sergey Davydov <sddevelop@ya.ru>
2024-05-25 07:17:10 +0300

    maybe can test config init ?

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	golang.org/x/net v0.24.0
 	golang.org/x/sync v0.7.0
 	golang.org/x/tools v0.20.0
+	gotest.tools/v3 v3.5.0
 	honnef.co/go/tools v0.4.7
 )
 
--- tests/server/config_test.go
+++ tests/server/config_test.go
@@ -2,11 +2,14 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/MrSwed/go-musthave-metrics/internal/server/config"
 	"github.com/MrSwed/go-musthave-metrics/tests"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -30,6 +33,44 @@ func (suite *ConfigTestSuite) TearDownSuite() {
 
 func TestConfigs(t *testing.T) {
 	suite.Run(t, new(ConfigTestSuite))
+}
+
+func (suite *ConfigTestSuite) TestInit() {
+	t := suite.T()
+	tests := []struct {
+		config map[string]any
+		flag   map[string]any
+		env    map[string]any
+		want   *config.Config
+		name   string
+	}{
+		{
+			config: nil,
+			flag:   nil,
+			env:    nil,
+			want:   config.NewConfig(),
+			name:   "Default",
+		},
+	}
+	for _, test := range tests {
+		if test.config != nil {
+			// prepare config file for test
+		}
+		if test.flag != nil {
+			// prepare flag for test
+		}
+		if test.env != nil {
+			// prepare env sets
+		}
+		t.Run(test.name, func(t *testing.T) {
+			var err error
+			cfg := config.NewConfig()
+			cfg, err = cfg.Init()
+			assert.NoError(t, err)
+			// cm := reflect.DeepEqual(cfg, test.want)
+			assert.Equal(t, true, reflect.DeepEqual(cfg, test.want), fmt.Sprintf("expected: %v\n actual: %v", test.want, cfg))
+		})
+	}
 }
 
 func (suite *ConfigTestSuite) TestLoadPrivateKey() {
