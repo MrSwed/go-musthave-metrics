cc81acebf1ebc162f72e4dd487b39cfa821507ce
Sergey Davydov <sddevelop@ya.ru>
2024-06-04 22:03:35 +0300

    refactors

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/MrSwed/go-musthave-metrics/internal/server/config"
 	"github.com/MrSwed/go-musthave-metrics/internal/server/constant"
-	myMiddleware "github.com/MrSwed/go-musthave-metrics/internal/server/middleware"
 	"github.com/MrSwed/go-musthave-metrics/internal/server/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -52,37 +51,37 @@ func setHeaderSHA(r http.ResponseWriter, key string, data []byte) {
 // Handler
 // init app routes
 func (h *Handler) HTTPHandler() http.Handler {
-	h.app.Use(myMiddleware.Logger(h.log))
+	h.app.Use(Logger(h.log))
 	h.app.Use(middleware.Compress(gzip.DefaultCompression, "application/json", "text/html"))
-	h.app.Use(myMiddleware.Decrypt(h.c.GetPrivateKey(), h.log))
-	h.app.Use(myMiddleware.Decompress(h.log))
-	h.app.Use(myMiddleware.CheckSign(h.c, h.log))
-	h.app.Use(myMiddleware.CheckNetwork(h.c, h.log))
+	h.app.Use(Decrypt(h.c.GetPrivateKey(), h.log))
+	h.app.Use(Decompress(h.log))
+	h.app.Use(CheckSign(h.c, h.log))
+	h.app.Use(CheckNetwork(h.c, h.log))
 
 	h.app.Mount("/debug", middleware.Profiler())
 
-	h.app.With(myMiddleware.TextHeader()).Route("/", func(r chi.Router) {
+	h.app.With(TextHeader()).Route("/", func(r chi.Router) {
 		r.Get("/", h.GetListMetrics())
 		r.Get("/ping", h.GetDBPing())
 	})
 
 	h.app.Route(constant.UpdateRoute, func(r chi.Router) {
-		r.With(myMiddleware.TextHeader()).Post(fmt.Sprintf("/{%s}/{%s}/{%s}",
+		r.With(TextHeader()).Post(fmt.Sprintf("/{%s}/{%s}/{%s}",
 			constant.MetricTypeParam, constant.MetricNameParam, constant.MetricValueParam),
 			h.UpdateMetric())
 
-		r.With(myMiddleware.JSONHeader()).Post("/", h.UpdateMetricJSON())
+		r.With(JSONHeader()).Post("/", h.UpdateMetricJSON())
 	})
 
 	h.app.Route(constant.UpdatesRoute, func(r chi.Router) {
-		r.With(myMiddleware.JSONHeader()).Post("/", h.UpdateMetrics())
+		r.With(JSONHeader()).Post("/", h.UpdateMetrics())
 	})
 
 	h.app.Route(constant.ValueRoute, func(r chi.Router) {
-		r.With(myMiddleware.TextHeader()).Get(fmt.Sprintf("/{%s}/{%s}",
+		r.With(TextHeader()).Get(fmt.Sprintf("/{%s}/{%s}",
 			constant.MetricTypeParam, constant.MetricNameParam), h.GetMetric())
 
-		r.With(myMiddleware.JSONHeader()).Post("/", h.GetMetricJSON())
+		r.With(JSONHeader()).Post("/", h.GetMetricJSON())
 	})
 
 	return h.app
