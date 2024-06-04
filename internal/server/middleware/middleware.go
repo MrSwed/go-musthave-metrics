24af25758d5f98f619f163ffe72a333d3c7d35af
Sergey Davydov <sddevelop@ya.ru>
2024-06-04 22:01:19 +0300

    refactors

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -8,12 +8,15 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"time"
 
 	"github.com/MrSwed/go-musthave-metrics/internal/server/config"
 	"github.com/MrSwed/go-musthave-metrics/internal/server/constant"
+	"github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
 )
 
@@ -140,6 +143,32 @@ func CheckNetwork(conf *config.WEB, l *zap.Logger) func(next http.Handler) http.
 				}
 			}
 			next.ServeHTTP(rw, r)
+		})
+	}
+}
+
+// Logger
+// middleware logger
+func Logger(l *zap.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			t1 := time.Now()
+			defer func() {
+				scheme := "http"
+				if r.TLS != nil {
+					scheme = "https"
+				}
+				l.Info("Served",
+					zap.Int("status", ww.Status()),
+					zap.String("method", r.Method),
+					zap.String("URI", fmt.Sprintf("%s://%s%s %s", scheme, r.Host, r.RequestURI, r.Proto)),
+					zap.Int("size", ww.BytesWritten()),
+					zap.Duration("time", time.Since(t1)),
+					zap.String("from", r.RemoteAddr),
+				)
+			}()
+			next.ServeHTTP(ww, r)
 		})
 	}
 }
