712695c6d091bb1bf1a1893adb0ceb194c790566
Sergey Davydov <sddevelop@ya.ru>
2024-06-11 09:21:39 +0300

    more cover

--- tests/agent/app_test.go
+++ tests/agent/app_test.go
@@ -3,11 +3,14 @@ package agent
 import (
 	"bytes"
 	"context"
+	"flag"
 	"fmt"
+	servApp "go-musthave-metrics/internal/server/app"
+	servConfig "go-musthave-metrics/internal/server/config"
 	"log"
+	"math/rand"
+	"net"
 	"os"
-	"os/signal"
-	"syscall"
 	"testing"
 	"time"
 
@@ -16,14 +19,15 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
 func Test_app_Run(t *testing.T) {
+
 	type fields struct {
-		ctx   context.Context
-		stop  context.CancelFunc
-		cfg   *config.Config
-		build app.BuildMetadata
+		cfg       *config.Config
+		buildInfo app.BuildMetadata
+		sCfg      *servConfig.Config
 	}
 	tests := []struct {
 		name             string
@@ -31,14 +35,14 @@ func Test_app_Run(t *testing.T) {
 		wantStrings      []string
 		doNotWantStrings []string
 	}{{
-		name: "Test_Run",
+		name: "Agent without server",
 		fields: func() fields {
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			cfg := config.NewConfig()
+			cfg.ReportInterval = 2
+			cfg.PollInterval = 1
 			return fields{
-				ctx:  ctx,
-				stop: cancel,
-				cfg:  config.NewConfig(),
-				build: app.BuildMetadata{
+				cfg: cfg,
+				buildInfo: app.BuildMetadata{
 					Version: "1.0-testing",
 					Date:    "24.05.24",
 					Commit:  "444333",
@@ -50,42 +54,189 @@ func Test_app_Run(t *testing.T) {
 			`BuildDate: 24.05.24`,
 			`BuildCommit: 444333`,
 			`Url for collect metric: http://localhost:8080/updates`,
-			`Report interval: 10`,
-			`Poll interval: 2`,
+			`Report interval: 2`,
+			`Poll interval: 1`,
 			`Rate limit: 1`,
 			`Number of metrics at once: 10`,
 			`Metric names count: 32`,
-			`daemon started: collect runtime metrics with interval 2`,
-			`daemon started: collect psutil metrics with interval 2`,
-			`daemon started: send metrics interval 10`,
+			`daemon started: collect runtime metrics with interval 1`,
+			`daemon started: collect psutil metrics with interval 1`,
+			`daemon started: send metrics interval 2`,
 			`Collect runtime metrics`,
 			`Collect psutil metrics`,
 			`PSUtil metrics collector is stopped`,
 			`Runtime metrics collector is stopped`,
-			`Metrics sender is stopped`,
+			`ctx done, do not try more`,
+			`second before next try`,
 			`Agent stopped`,
 		},
 	},
+		{
+			name: "Agent grpc without server",
+			fields: func() fields {
+				cfg := config.NewConfig()
+				cfg.ReportInterval = 2
+				cfg.PollInterval = 1
+				cfg.GRPCAddress = net.JoinHostPort("localhost", fmt.Sprintf("%d", rand.Intn(200)+20000))
+				return fields{
+					cfg: cfg,
+					buildInfo: app.BuildMetadata{
+						Version: "1.0-testing",
+						Date:    "24.05.24",
+						Commit:  "444333",
+					},
+				}
+			}(),
+			wantStrings: []string{
+				`BuildVersion: 1.0-testing`,
+				`BuildDate: 24.05.24`,
+				`BuildCommit: 444333`,
+				`Url for collect metric: http://localhost:8080/updates`,
+				`Report interval: 2`,
+				`Poll interval: 1`,
+				`Rate limit: 1`,
+				`Number of metrics at once: 10`,
+				`Metric names count: 32`,
+				`daemon started: collect runtime metrics with interval 1`,
+				`daemon started: collect psutil metrics with interval 1`,
+				`daemon started: send metrics interval 2`,
+				`Collect runtime metrics`,
+				`Collect psutil metrics`,
+				`PSUtil metrics collector is stopped`,
+				`Runtime metrics collector is stopped`,
+				`transport: Error while dialing`,
+				`Metrics sender is stopped`,
+				`Agent stopped`,
+			},
+		},
+		{
+			name: "Agent and server",
+			fields: func() fields {
+
+				servCfg := servConfig.NewConfig()
+				servCfg.StorageConfig.FileStoragePath = ""
+				servCfg.Address = net.JoinHostPort("localhost", fmt.Sprintf("%d", rand.Intn(200)+20000))
+				servCfg.GRPCAddress = ""
+				// servCfg.GRPCAddress = net.JoinHostPort("", fmt.Sprintf("%d", rand.Intn(200)+30000))
+
+				cfg := config.NewConfig()
+				cfg.ReportInterval = 2
+				cfg.PollInterval = 1
+				cfg.Address = servCfg.Address
+
+				return fields{
+					cfg:  cfg,
+					sCfg: servCfg,
+					buildInfo: app.BuildMetadata{
+						Version: "1.1-testing",
+						Date:    "24.05.24",
+						Commit:  "4444444",
+					},
+				}
+			}(),
+			wantStrings: []string{
+				`BuildVersion: 1.1-testing`,
+				`BuildDate: 24.05.24`,
+				`BuildCommit: 4444444`,
+				`Url for collect metric: http://localhost`,
+				`Report interval: 2`,
+				`Poll interval: 1`,
+				`Rate limit: 1`,
+				`Number of metrics at once: 10`,
+				`Metric names count: 32`,
+				`daemon started: collect runtime metrics with interval 1`,
+				`daemon started: collect psutil metrics with interval 1`,
+				`daemon started: send metrics interval 2`,
+				`Collect runtime metrics`,
+				`Collect psutil metrics`,
+				`PSUtil metrics collector is stopped`,
+				`Runtime metrics collector is stopped`,
+				`Metrics sender is stopped`,
+				`metrics sent`,
+				`Agent stopped`,
+			},
+		},
+		{
+			name: "Agent and server GRPC",
+			fields: func() fields {
+
+				servCfg := servConfig.NewConfig()
+				servCfg.StorageConfig.FileStoragePath = ""
+				servCfg.Address = net.JoinHostPort("localhost", fmt.Sprintf("%d", rand.Intn(200)+20000))
+				servCfg.GRPCAddress = net.JoinHostPort("", fmt.Sprintf("%d", rand.Intn(200)+30000))
+				servCfg.GRPCToken = "secretToken"
+
+				cfg := config.NewConfig()
+				cfg.ReportInterval = 2
+				cfg.PollInterval = 1
+				cfg.GRPCAddress = servCfg.GRPCAddress
+				cfg.GRPCToken = servCfg.GRPCToken
+
+				return fields{
+					cfg:  cfg,
+					sCfg: servCfg,
+					buildInfo: app.BuildMetadata{
+						Version: "1.1-testing",
+						Date:    "24.05.24",
+						Commit:  "4444444",
+					},
+				}
+			}(),
+			wantStrings: []string{
+				`BuildVersion: 1.1-testing`,
+				`BuildDate: 24.05.24`,
+				`BuildCommit: 4444444`,
+				`Url for collect metric: http://localhost`,
+				`Report interval: 2`,
+				`Poll interval: 1`,
+				`Rate limit: 1`,
+				`Number of metrics at once: 10`,
+				`Metric names count: 32`,
+				`daemon started: collect runtime metrics with interval 1`,
+				`daemon started: collect psutil metrics with interval 1`,
+				`daemon started: send metrics interval 2`,
+				`Collect runtime metrics`,
+				`Collect psutil metrics`,
+				`PSUtil metrics collector is stopped`,
+				`Runtime metrics collector is stopped`,
+				`Metrics sender is stopped`,
+				`metrics sent`,
+				`Agent stopped`,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx, stop := signal.NotifyContext(tt.fields.ctx, os.Interrupt, syscall.SIGTERM)
-			defer stop()
-			defer tt.fields.stop()
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
+			oldArgs := os.Args
+			os.Args = os.Args[:1]
+			defer func() { os.Args = oldArgs }()
 
 			require.NoError(t, tt.fields.cfg.Init())
-
-			appHandler := app.NewApp(ctx, tt.fields.cfg, tt.fields.build)
 
 			var buf bytes.Buffer
 			log.SetOutput(&buf)
 			defer func() {
 				log.SetOutput(os.Stderr)
 			}()
+			var sApp *servApp.App
+			if tt.fields.sCfg != nil {
+				sApp = servApp.NewApp(ctx, cancel,
+					servApp.BuildMetadata{Version: "testing..", Date: time.Now().String(), Commit: ""},
+					tt.fields.sCfg, zap.NewNop())
+				go sApp.Run()
+			}
 
+			appHandler := app.NewApp(ctx, tt.fields.cfg, tt.fields.buildInfo)
 			appHandler.Run()
 			appHandler.Stop()
 
+			if sApp != nil {
+				sApp.Stop()
+			}
 			t.Log(buf.String())
 			for i := 0; i < len(tt.wantStrings); i++ {
 				assert.Contains(t, buf.String(), tt.wantStrings[i], fmt.Sprintf("%s is expected at log out", tt.wantStrings[i]))
